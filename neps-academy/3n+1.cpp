109ba642d9c57eb476807dd63cdd79fe3c4ae834
Sinhoroto <bruno.sinhoroto@hotmail.com>
2020-09-27 12:54:58 -0300

    codes

---
+++ neps-academy/3n+1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, count = 0;
+    cin >> n;
+    while (n != 1)
+    {
+        if (n%2 ==0)
+        {
+            n = n/2;
+            count++;
+        }else
+        {
+            n = 3*n+1;
+            count++;
+        }
+        
+    }
+    
+    cout << count << endl;
+    return 0;
+}
---
+++ neps-academy/Basquete de Robos.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int D;
+    cin >> D;
+    if(D<=800)
+        cout<<"1";
+    else if(D> 800 && D<=1400)
+        cout<<"2";
+    else
+        cout<< "3";
+}
